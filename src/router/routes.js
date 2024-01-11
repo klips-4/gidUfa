2ae0276651a6c063d11158a694e1bef6fa424dcb
klips-4 <klips-4@yandex.ru>
2024-01-11 19:53:25 +0500

    add descriptions 2

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,9 @@
 import MainPage from '@/pages/MainPage.vue'
+import Descriptions from '@/components/Descriptions.vue'
 
 const routes = [
     {path: '/', component: MainPage},
+    {path: '/descriptions/:page', component: Descriptions}
 ]
 
 export default routes
