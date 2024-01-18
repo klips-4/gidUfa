36b759cae5648ca9374df6296b9ce541b9ec5650
CaptainTelepuzik <106181698+CaptainTelepuzik@users.noreply.github.com>
2024-01-18 21:15:36 +0500

    w

---
+++ src/components/ListContent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="description-store">
+
+    <div v-for="(item, index) in descriptionStore.descriptions.data"
+         :key="index"
+         @click="showId(item.id)"
+         >
+        <h2 class="item-name">{{item}}</h2>
+      </div>
+
+  </div>
+</template>
+
+<script setup>
+//"Подложка" для подробного описания элементов из CityInformationsCards.vue
+
+import {useDescriptionStore} from "../store/description-store";
+
+const descriptionStore = useDescriptionStore()
+descriptionStore.fetchDescriptionData("fountains")
+
+const showId = (index) => {
+  console.log(index)
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -2,12 +2,15 @@
   <Header/>
   <Content/>
   <Footer/>
+  <ListContent/>
+
 </template>
 
 <script setup>
 
 import Header from "@/components/Header.vue";
 import Content from "@/components/Content.vue";
+import ListContent from "@/components/ListContent.vue";
 import Footer from "@/components/Footer.vue";
 </script>
 
